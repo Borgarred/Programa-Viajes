ec1a45b3a841cbd3d2f105303dcd225b742f87f2
lopezordonezrebeca <60574419+lopezordonezrebeca@users.noreply.github.com>
2023-05-11 16:36:29 +0200

    Update menu_principal.java

--- Aplicación_Viajes/src/menu_principal.java
+++ Aplicación_Viajes/src/menu_principal.java
@@ -1,6 +1,6 @@
 package Aplicacion_Viajes;
 
-//HERENCIA
+//La clase menu_principal hereda de Usuario
 public class menu_principal extends Usuario {
 
 	int opcion1;
@@ -14,14 +14,14 @@ public class menu_principal extends Usuario {
 	}
 
 	
-	public void MenuInicio() { // METODO
+	public void MenuInicio() { //Este metodo sale al principio de la aplicación siempre.
 
 		System.out.print("------MENÚ INICIO------" + "\n1. Registrar Nuevo Usuario." + "\n2. Iniciar Sesión."
 				+ "\n\nEscoja una opción: ");
 		this.opcion1 = sc.nextInt();
 	}
 
-	public void MenuViajes(int indice, Usuario user) { // METODO
+	public void MenuViajes(int indice, Usuario user) { //Metodo que al iniciar sesión correctamente sale por pantalla
 
 		System.out.print("¡Hola! " + user.arrayUsuarios[indice].getNombre() + " elige una de nuestras opciones: "
 				+ " \n(1)-Viajar \n(2)-Recomendaciones \n(3)-Sorteo \n(4)-Salir" + "\n\nEscoja una opción:  ");
